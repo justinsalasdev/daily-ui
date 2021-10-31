6fb2e1c82462169fe11461605930e352d558a653
justinsalasdev <justinsalasdev@gmail.com>
2021-10-31 13:19:31 +0800

    implement millis step

--- src/Clock/useClock.ts
+++ src/Clock/useClock.ts
@@ -24,7 +24,8 @@ export default function useClock() {
 
 function getPos(date: Date): Position {
   // console.log(date.getSeconds(), date.getMinutes(), date.getHours());
-  const sec = (date.getSeconds() / 60) * 360;
+  const millis = date.getUTCMilliseconds() / 1000;
+  const sec = (date.getSeconds() / 60) * 360 + millis * 6;
   const min = (date.getMinutes() / 60) * 360 + (sec / 360) * 6;
   const hour = (date.getHours() / 12) * 360 + (min / 360) * 30;
   return { sec, min, hour };
